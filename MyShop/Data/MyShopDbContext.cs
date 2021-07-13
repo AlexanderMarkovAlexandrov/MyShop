dd31e45a253f9eaf9ac4a844dfbe45b382611a99
AlexandarMarkovIT <alex.markov2013@gmail.com>
2021-07-13 17:11:09 +0300

    change folders

---
+++ MyShop/Data/MyShopDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace MyShop.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    public class MyShopDbContext : IdentityDbContext
+    {
+        public MyShopDbContext(DbContextOptions<MyShopDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
